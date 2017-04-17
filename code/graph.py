18531ff00bf9f6bc47c0dd922f84f867705b25a1
System Administrator <root@prateeks-mbp.attlocal.net>
2017-04-17 02:25:13 -0500

    result for hidden 3

--- code/graph.py
+++ code/graph.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 
-fileDir = "../hidden_2/epochLossFile.txt"
+fileDir = "../hidden_3/batchLossFile.txt"
 
 with open(fileDir,'r') as g:
 	text = g.readlines()
 
-plt.plot(range(len(text)),text,'-bo', label = "lr : 0.001")
+plt.plot(range(len(text)),text,label = "lr_0.001 : clip_10 : steps_128")
 plt.ylabel("loss")
-plt.xlabel("epoch")
+plt.xlabel("batch")
 plt.legend()
-plt.savefig("../hidden_2/lr-0.001.png")
+plt.savefig("../hidden_3/result_2.png", dpi = 500)
