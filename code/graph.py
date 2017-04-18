1a0d04e4d862cf41c1dc9c49ea6e67348fdb4534
System Administrator <root@prateeks-mbp.attlocal.net>
2017-04-17 23:36:38 -0500

    graph gor hidden 1

--- code/graph.py
+++ code/graph.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 
-fileDir = "../hidden_3_lr_0.001_clip_100_step_128/epochLossFile.txt"
+fileDir = "../hidden_1_lr_0.001_clip_100_steps_128/batchLossFile.txt"
 
 with open(fileDir,'r') as g:
 	text = g.readlines()
 
-plt.plot(range(len(text)),text,label = "hidden_3_lr_0.001_clip_100_step_128")
+plt.plot(range(len(text)),text,label = "hidden_1_lr_0.001_clip_100_steps_128")
 plt.ylabel("loss")
-plt.xlabel("epoch")
+plt.xlabel("batch")
 plt.legend()
-plt.savefig("../hidden_3_lr_0.001_clip_100_step_128/result_2.png", dpi = 500)
+plt.savefig("../hidden_1_lr_0.001_clip_100_steps_128/batchLoss.png", dpi = 500)
